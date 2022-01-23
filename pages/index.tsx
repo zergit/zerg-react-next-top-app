e84810dfb663bc7de229bd1c7c8ab63a64b60134
Sergey Igrunin <igrunen@bk.ru>
2022-01-23 20:01:19 +0300

    svg, webpack

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 //import Image from 'next/image';
 import React from 'react';
-import { Htag } from '../components';
+import { Button, Htag } from '../components';
 
 
 export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag='h1'>Заголовок</Htag>
+      <Button appearance='primary' arrow='right'>Кнопка</Button>
+      <Button appearance='ghost' arrow='down'>Кнопка</Button>
     </>
   );
 }
