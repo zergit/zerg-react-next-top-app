b4c1efa9f43d770818c35fbe6fdd043ad3ec4161
Sergey Igrunin <igrunen@bk.ru>
2022-01-24 16:35:39 +0300

    простые компоненты

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 //import Image from 'next/image';
 import React from 'react';
-import { Button, Htag } from '../components';
+import { Button, Htag, P, Tag } from '../components';
 
 
 export default function Home(): JSX.Element {
@@ -9,6 +9,13 @@ export default function Home(): JSX.Element {
       <Htag tag='h1'>Заголовок</Htag>
       <Button appearance='primary' arrow='right'>Кнопка</Button>
       <Button appearance='ghost' arrow='down'>Кнопка</Button>
+      <P size='l'>Большой</P>
+      <P>Средний</P>
+      <P size='s'>Маленький</P>
+      <Tag size='s'>Ghost</Tag>
+      <Tag size='m' color='red'>Red</Tag>
+      <Tag size='s' color='green'>Green</Tag>
+      <Tag color='primary' >Green</Tag>
     </>
   );
 }
