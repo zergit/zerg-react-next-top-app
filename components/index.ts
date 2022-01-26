eef397efbbbc42d8761d4bdf6988f3ec5fbd4ef5
Sergey Igrunin <igrunen@bk.ru>
2022-01-26 14:45:41 +0300

    Rating особое внимание

--- components/index.ts
+++ components/index.ts
@@ -2,4 +2,5 @@ export * from './Htag/Htag';
 export * from './Button/Button';
 export * from './P/P';
 export * from './Tag/Tag';
+export * from './Rating/Rating';
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 //import Image from 'next/image';
-import React from 'react';
-import { Button, Htag, P, Tag } from '../components';
+import React, { useState } from 'react';
+import { Button, Htag, P, Rating, Tag } from '../components';
 
 
 export default function Home(): JSX.Element {
+  const [rating, setRating] = useState<number>(4);
+
+
   return (
     <>
       <Htag tag='h1'>Заголовок</Htag>
@@ -16,6 +19,7 @@ export default function Home(): JSX.Element {
       <Tag size='m' color='red'>Red</Tag>
       <Tag size='s' color='green'>Green</Tag>
       <Tag color='primary' >Green</Tag>
+      <Rating rating={rating} isEditable setRating={setRating} />
     </>
   );
 }
